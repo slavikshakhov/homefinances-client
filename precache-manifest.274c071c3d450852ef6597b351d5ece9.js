b2bad3c03e72356ae04113e555ed518f20ede7a2
slavikshakhov <v.shakhov@yahoo.ca>
2020-10-19 13:13:22 +0200

    Updates

---
+++ precache-manifest.274c071c3d450852ef6597b351d5ece9.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49ad93af845a12e5316133051d2db3ae",
+    "url": "/homefinances-client/index.html"
+  },
+  {
+    "revision": "235f41e2e3c85156ed4b",
+    "url": "/homefinances-client/static/css/2.2a60accc.chunk.css"
+  },
+  {
+    "revision": "c054c7bde15aca3ff26a",
+    "url": "/homefinances-client/static/css/main.20713a1c.chunk.css"
+  },
+  {
+    "revision": "235f41e2e3c85156ed4b",
+    "url": "/homefinances-client/static/js/2.4b34a6c5.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/homefinances-client/static/js/2.4b34a6c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c054c7bde15aca3ff26a",
+    "url": "/homefinances-client/static/js/main.3d70ceb7.chunk.js"
+  },
+  {
+    "revision": "b012e4d026f01a8b71f4",
+    "url": "/homefinances-client/static/js/runtime-main.006d565a.js"
+  },
+  {
+    "revision": "75f1b02987ca56053572ea3b6b506768",
+    "url": "/homefinances-client/static/media/home-bg.75f1b029.jpg"
+  },
+  {
+    "revision": "c2a1d2442079c9924fb172642e12b988",
+    "url": "/homefinances-client/static/media/pig.c2a1d244.jpg"
+  }
+]);
